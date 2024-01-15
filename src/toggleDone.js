5c763e464cd05c1deabe51b731e497ee03024026
BenjaminJMcD <BenjaminJMcD@outlook.com>
2024-01-15 13:15:09 -0600

    donecheckbox edits item

--- src/toggleDone.js
+++ src/toggleDone.js
@@ -1,10 +1,25 @@
+import findItem from "./findItem";
+import editItem from "./editItem";
+
 export default function toggleDone(event) {
 
-    if (event.target.checked) {
-        event.target.parentElement.classList.add("done");
+    const item = event.target;
+
+    const parentElement = item.parentNode;
+    
+    const itemName = parentElement.firstChild.innerText.slice(2);
+
+    const itemEdit = findItem(itemName);
+
+    if (item.checked) {
+        item.parentElement.classList.add("done");
+        itemEdit.done = true;
     }
-    else if (!event.target.checked) {
-        event.target.parentElement.classList.remove("done");
+    else if (!item.checked) {
+        item.parentElement.classList.remove("done");
+        itemEdit.done = false;
     }
+
+    editItem(itemEdit.key, itemEdit, itemEdit.list);
 
 }
